1e6e55e54ffdaab027306b8cefcd1087d8ba6862
William Banfield <wbanfield@gmail.com>
2022-05-24 06:09:39 -0400

    add basic provider

---
+++ tf/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "1.22.2"
+    }
+  }
+}
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}

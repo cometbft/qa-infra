c9c22c383784994c3858078a2ce31de39d64f744
William Banfield <wbanfield@gmail.com>
2022-05-24 06:26:41 -0400

    basic node config

--- tf/node.tf
+++ tf/node.tf
@@ -1,44 +1,8 @@
-resource "google_compute_address" "static" {
-  name = "ipv4-address"
-  region  = "europe-west6"
-}
-
-data "google_compute_image" "centos_image" {
-  family  = "centos-7"
-  project = "centos-cloud"
-}
-
-resource "google_compute_disk" "default" {
-  name  = "evmos-disk"
-  type  = "pd-standard"
-  zone  = "europe-west6-a"
-  size  = "200"
-}
-
-
-resource "google_compute_instance" "instance" {
-  name         = "evmos-validator"
-  machine_type = "e2-standard-4"
-  zone         = "europe-west6-a"
-  tags = ["evmos"]
-
-  boot_disk {
-    initialize_params {
-      image = "centos-cloud/centos-7"
-    }
-  }
-
-  attached_disk {
-    source = google_compute_disk.default.self_link
-    device_name = "evmos_disk"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      nat_ip = google_compute_address.static.address
-    }
-  }
+resource "digitalocean_droplet" "v0.36-testnet-node" {
+  name         = "v0.36-testnet-node"
+  image        = "debian-11-x64"
+  region       = "fra1"
+  tags = ["v0.36-testnet"]
+  size = "s-4vcpu-8gb"
 }
 

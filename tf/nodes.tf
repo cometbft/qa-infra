c15724ef4135aa05501a182eb4516df465903135
William Banfield <wbanfield@gmail.com>
2022-05-31 03:35:48 -0400

    add prom node

--- tf/nodes.tf
+++ tf/nodes.tf
@@ -12,9 +12,18 @@ variable "instance_tags" {
 	default = ["v036-testnet"]
 }
 
-resource "digitalocean_droplet" "v036-testnet-node" {
+resource "digitalocean_droplet" "testnet-node" {
   count        = var.testnet_size
   name         = "validator${count.index}"
+  image        = "debian-11-x64"
+  region       = "fra1"
+  tags = var.instance_tags
+  size = "s-4vcpu-8gb"
+  ssh_keys = var.ssh_keys
+}
+
+resource "digitalocean_droplet" "testnet-prometheus" {
+  name         = "test-prometheus"
   image        = "debian-11-x64"
   region       = "fra1"
   tags = var.instance_tags

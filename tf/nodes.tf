aed6e9e73e1dc471be07076c920743f6d152599b
William Banfield <wbanfield@gmail.com>
2022-06-08 10:19:26 -0400

    terraform fixes

--- tf/nodes.tf
+++ tf/nodes.tf
@@ -1,36 +1,21 @@
-variable "testnet_size" {
-  type = number
-  default = 20
-}
-
 variable "ssh_keys" {
 	type = list(string)
 }
 
-variable "instance_tags" {
-	type = list(string)
-	default = ["v036-testnet"]
-}
-
-variable "instance_names" {
-	type = list(string)
-}
-
-resource "digitalocean_droplet" "testnet-node" {
-  count        = var.testnet_size
+resource "digitalocean_droplet" "celestia-node" {
   name         = var.instance_names[count.index]
   image        = "debian-11-x64"
   region       = "fra1"
-  tags = concat(var.instance_tags, ["testnet-node"])
+  tags =  ["celestia", "celestia-node"]
   size = "s-4vcpu-8gb"
   ssh_keys = var.ssh_keys
 }
 
 resource "digitalocean_droplet" "testnet-prometheus" {
-  name         = "testnet-prometheus"
+  name         = "celestia-prometheus"
   image        = "debian-11-x64"
   region       = "fra1"
-  tags = concat(var.instance_tags, ["testnet-observability"])
+  tags =  ["celestia", "celestia-observability"]
   size = "s-4vcpu-8gb"
   ssh_keys = var.ssh_keys
 }
--- tf/project.tf
+++ tf/project.tf
@@ -1,5 +1,5 @@
-resource "digitalocean_project" "tm-testnet" {
-  name        = "tm-testnet"
-  description = "A project to test the Tendermint codebase."
-  resources = concat([for node in digitalocean_droplet.testnet-node: node.urn], [digitalocean_droplet.testnet-prometheus.urn])
+resource "digitalocean_project" "celestia-node" {
+  name        = "celestia-node"
+  description = "A project to test the Tendermint running with Celestia."
+  resources = [digitalocean_droplet.celestia-node.urn, digitalocean_droplet.testnet-prometheus.urn]
 }

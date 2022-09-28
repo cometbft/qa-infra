15dbd769996eb7ed0c2f1741a750b3f680b842d9
William Banfield <wbanfield@gmail.com>
2022-09-28 11:19:50 -0400

    edits for v034

--- tf/project.tf
+++ tf/project.tf
@@ -1,4 +1,4 @@
-resource "digitalocean_project" "tm-testnet" {
+resource "digitalocean_project" "tm-testnet-v034" {
   name        = "tm-testnet"
   description = "A project to test the Tendermint codebase."
   resources   = concat([for node in digitalocean_droplet.testnet-node: node.urn], [digitalocean_droplet.testnet-prometheus.urn], [digitalocean_droplet.testnet-load-runner.urn], [for node in digitalocean_droplet.ephemeral-node: node.urn])

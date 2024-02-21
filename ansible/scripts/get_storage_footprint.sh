af5f450a29f54294d12254391106f38a83c3ba4f
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2024-02-21 13:13:48 +0100

    Parametrize CMT_HOME

--- ansible/scripts/get_storage_footprint.sh
+++ ansible/scripts/get_storage_footprint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-while true ; do du -m /root/.testapp/data | tee -a /root/.testapp/folder_size.log ; sleep 10 ; done
+CMT_HOME=$1
+
+if [ -z "${CMT_HOME}" ]; then
+CMT_HOME="/root/.testapp"
+fi 
+
+while true ; do du -m $CMT_HOME/data | tee -a $CMT_HOME/folder_size.log ; sleep 10 ; done

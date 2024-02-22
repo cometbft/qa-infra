c4656a3d0d3d6e6fa3dffa18ca48b37ad9324598
Hernán Vanzetto <15466498+hvanz@users.noreply.github.com>
2024-02-22 14:23:00 +0100

    refactor: Move VERSION_TAG variable from Makefile to experiment.mk (#44)
    * Define path to manifest file in a variable
    
    * Move VERSION_TAG variable from Makefile to experiment.mk
    
    * Update experiment.mk
    
    Co-authored-by: lasaro <lasaro@gmail.com>
    
    ---------
    
    Co-authored-by: lasaro <lasaro@gmail.com>

--- experiment.mk
+++ experiment.mk
@@ -3,6 +3,20 @@
 DO_INSTANCE_TAGNAME=main-testnet
 DO_VPC_SUBNET=172.19.144.0/20
 
+MANIFEST=$(realpath ./testnets/example.toml)
+
+VERSION_TAG ?= f92bace91 # tag of main on 05.02.2024
+#VERSION_TAG ?= 3b783434f #v0.34.27 (cometbft/cometbft)
+#VERSION_TAG ?= bef9a830e  #v0.37.alpha3 (cometbft/cometbft)
+#VERSION_TAG ?= v0.38.0-alpha.2
+#VERSION_TAG ?= e9abb116e #v0.38.alpha2 (cometbft/cometbft)
+
+VERSION_WEIGHT ?= 1
+
+## For testnets that mix nodes running different versions of CometBFT.
+#VERSION2_TAG ?= 66c2cb634 #v0.34.26 (informalsystems/tendermint)
+VERSION2_WEIGHT ?= 0
+
 EPHEMERAL_SIZE ?= 0
 
 MANIFEST=$(realpath ./testnets/example.toml)
